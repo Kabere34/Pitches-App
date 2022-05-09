0c6a6378f78dcf6bd02c81a05b2d85e5b1f11cf3
Ivy Kabere <kabereivy@gmail.com>
2022-05-09 19:21:19 +0300

    ft:create a regristation form

--- pitch.py
+++ pitch.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template, url_for
+from forms import RegistrationForm,LoginForm
 
 app=Flask(__name__)
+
+app.config['SECRET_KEY'] ='03ab733539228fc6a292092770c88c4a'
 
 posts=[
   {
@@ -25,6 +28,18 @@ def home():
 @app.route("/pickup")
 def about():
     return render_template('pickup.html')
+
+
+@app.route("/register")
+def register():
+  form = RegistrationForm()
+  return render_template('register.html',title='Register', form=form)
+
+@app.route("/login")
+def login():
+  form = LoginForm()
+  return render_template('login.html',title='Login', form=form)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
