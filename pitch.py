f48bcbae125954a8bab27b71bb0bb9b580cab9dd
Ivy Kabere <kabereivy@gmail.com>
2022-05-09 16:38:06 +0300

    ft:add navbar

--- pitch.py
+++ pitch.py
@@ -1,10 +1,26 @@
 from flask import Flask,render_template
 
-app = Flask(__name__)
+app=Flask(__name__)
+
+posts=[
+  {
+    'author': 'John',
+    'title':'blog post',
+    'content':'First post',
+    'date_posted':'April 14, 2014',
+  },
+  {
+    'author': 'Jane',
+    'title':'video post',
+    'content':'Second post',
+    'date_posted':'July 14, 2015',
+  }
+]
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html',posts=posts)
 
 @app.route("/pickup")
 def about():
