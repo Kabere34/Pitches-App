674de03366c6180213dfc1e390ae2440268aaa04
Ivy Kabere <kabereivy@gmail.com>
2022-05-10 17:42:54 +0300

    ft-add bcrypt extension to assist in password verification

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app=Flask(__name__)
 
@@ -8,5 +8,6 @@ app.config['SECRET_KEY'] ='03ab733539228fc6a292092770c88c4a'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
 db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitch import routes
