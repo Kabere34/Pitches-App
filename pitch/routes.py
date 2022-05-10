4c4a1c1aa5b8a64ea6318d01274d515efd998706
Ivy Kabere <kabereivy@gmail.com>
2022-05-10 21:51:07 +0300

    add function and route to handle user logout

--- pitch/routes.py
+++ pitch/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash,redirect
 from pitch import app, db, bcrypt
 from pitch.forms import RegistrationForm,LoginForm
 from pitch.models import User, Post
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 posts=[
   {
@@ -30,7 +30,7 @@ def about():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
-  if current_user.is_authenticated():
+  if current_user.is_authenticated:
     return redirect(url_for('home'))
   form = RegistrationForm()
   if form.validate_on_submit():
@@ -45,7 +45,7 @@ def register():
 
 @app.route("/login",methods=['GET','POST'])
 def login():
-  if current_user.is_authenticated():
+  if current_user.is_authenticated:
     return redirect(url_for('home'))
   form = LoginForm()
   if form.validate_on_submit():
@@ -57,3 +57,8 @@ def login():
       flash('Login unseccessful. Please check your email and password','danger')
 
   return render_template('login.html',title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for('home'))
